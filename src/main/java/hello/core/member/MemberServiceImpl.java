c0bfd370ec95f69ecfd9ee85026377ed812935db
dawwson <shines1427@gmail.com>
2023-08-17 20:44:05 +0900

    Comment: MemberServiceImpl 주석 추가

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     // 다형성에 의해서 MemoryMemberRepository에 구현된 save, findMember가 호출된다.
+    // 문제점 : 구상화에도 의존하고 추상화에도 의존하고 있다.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
