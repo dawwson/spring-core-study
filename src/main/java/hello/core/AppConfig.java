1227143d78649995f9d93186000cc9bf6a9ae5b6
dawwson <shines1427@gmail.com>
2023-08-17 23:15:02 +0900

    Refactor: AppConfig에 역할과 구현 클래스가 한 눈에 보이도록 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,12 +12,21 @@ import hello.core.order.OrderServiceImpl;
 // 어떤 구현체를 넣어줄지(주입, injection)는 외부 AppConfig에서 결정한다. => DIP 만족
 // 서비스 단에서는 어떤 구현체가 들어오는지 알 필요가 없고, 비즈니스 로직 실행에만 집중하면 된다. => 관심사 분리
 // 의존 관계를 외부에서 주입하는 것을 DI(Dependency Injection)이라고 한다.
+// 역할과 구현 클래스 한 눈에 보임(애플리케이션 전체 구성이 한 눈에 보임)
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
