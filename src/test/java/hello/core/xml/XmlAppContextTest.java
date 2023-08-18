3e2407ed8288b6ef1e3ecd98c6a55af53b14c26c
dawwson <shines1427@gmail.com>
2023-08-18 22:06:00 +0900

    Test: XML 파일로 빈 조회 테스트 작성

---
+++ src/test/java/hello/core/xml/XmlAppContextTest.java
@@ -0,0 +1,17 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+// xml 기반으로 설정 정보를 작성하는 것은최근에 잘 사용하지는 않음.
+public class XmlAppContextTest {
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
