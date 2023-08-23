ecbf92dcdcb5a3b3ef316b10c31b73b9b5d53653
dawwson <shines1427@gmail.com>
2023-08-23 18:02:30 +0900

    Test: Autowired 옵션 테스트

---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,42 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import jakarta.annotation.Nullable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    @Component
+    static class TestBean {
+
+        // 자동 주입할 대상이 없으면 수정자 메서드 자체가 호출이 안 된다.
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {  // 스프링 빈으로 관리되지 않는 클래스를 암거나 집어넣음
+            System.out.println("noBean1 = " + noBean1);
+
+        }
+
+        // null
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+
+        }
+
+        // Optional.empty
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+}
