a5020b1b09bcafba16e6f5d6ee3b647da422a1b5
dawwson <shines1427@gmail.com>
2023-08-23 15:55:23 +0900

    Comment: SpringBootApplication 어노테이션에 대한 주석 추가

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,10 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(
-                type = FilterType.ANNOTATION,
-                classes = Configuration.class  // => 예제에 있던 @Configuration 클래스들 제외
-        )
+        excludeFilters =
+                @ComponentScan.Filter(
+                        type = FilterType.ANNOTATION,
+                        classes = Configuration.class  // => 예제에 있던 @Configuration 클래스들 제외
+                )
 )
 public class AutoAppConfig {
 
--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,6 +3,8 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 이 어노테이션이 내부적으로 @ComponentScan, @Configuration 을 가지고 있기 때문에
+// 스프링 부트로 구동시 스프링 컨테이너를 실행하고 스프링 빈을 자동으로 등록한다.
 @SpringBootApplication
 public class CoreApplication {
 	public static void main(String[] args) {
