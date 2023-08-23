4946388d5892aca9fae3f110637bdb06eba2bb73
dawwson <shines1427@gmail.com>
2023-08-23 18:23:03 +0900

    Refactor: lombok 라이브러리 사용하도록 리팩터

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,20 +1,21 @@
 package hello.core.member;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor  // => final 이 붙은 필드를 모아서 생성자를 자동으로 만들어준다.
 public class MemberServiceImpl implements MemberService {
     // 1과 2를 통해 OCP, DIP 만족
     // 1. 추상에만 의존하도록 수정
     private final MemberRepository memberRepository;
-
+/*
     // 2. 생성자를 통해 의존성을 주입하도록 수정
     @Autowired  // 스프링이 컴포넌트 스캔으로 알아서 빈으로 등록하고 의존성 주입해줌
     public MemberServiceImpl(MemberRepository memberRepository) { // 생성자 주입
         this.memberRepository = memberRepository;
     }
-
+*/
     @Override
     public void join(Member member) {
         memberRepository.save(member);
